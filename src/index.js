d14fbcb6e88deb69129f6bdaad49a7dcf79538a4
Christopher Corvo <christophercorvo@gmail.com>
2020-10-28 19:34:16 -0400

    finished Tasks 1-3

--- src/index.js
+++ src/index.js
@@ -10,14 +10,20 @@ import axios from "axios";
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
+// ------------ imported hooks ---------------------
+import {useDarkMode} from './hooks/useDarkMode'
+
 // ------------- import styles -----------------
 import "./styles.scss";
 
-//
+// ----------- App Component --------------
+
 const App = () => { // this component is the brain of the application 
   // ------------ State Management -------------
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
+
+  
 
 // this is a side effect doing a get request and is setting the state of coinData with the api data
   useEffect(() => {
